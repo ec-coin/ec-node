b58785a13429ad3df7914656bc3de37c902c26ac
dylan <qvrqgjql@qwertymail.nl>
2021-09-28 14:54:57 +0200

    first commit

---
+++ src/main/java/nl/hanze/ec/network/Application.java
@@ -0,0 +1,8 @@
+package nl.hanze.ec.network;
+
+public class Application {
+    public void run() {
+        System.out.println("Hello world!");
+
+    }
+}
---
+++ src/main/java/nl/hanze/ec/network/runner/ECNetworkNodeRunner.java
@@ -0,0 +1,21 @@
+package nl.hanze.ec.network.runner;
+
+import nl.hanze.ec.network.Application;
+
+/**
+ * Hanzehogeschool Groningen University of Applied Sciences HBO-ICT
+ * Project monetair systeem
+ * EC Network Node
+ *
+ * @author Roy Voetman
+ * @author Dylan Hiemstra
+ * @author Wouter Folkertsma
+ * @author Jordi Mellema
+ *
+ * @version 1.0
+ */
+public class ECNetworkNodeRunner {
+    public static void main(String[] args) {
+        new Application().run();
+    }
+}
