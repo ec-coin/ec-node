d0d9de3188454cab5ebbc80bbf1419ea725570ef
dylode <78496445+dylode@users.noreply.github.com>
2021-10-10 10:52:53 +0200

    Update ApplicationTest.java

--- src/test/java/nl/hanze/ec/node/ApplicationTest.java
+++ src/test/java/nl/hanze/ec/node/ApplicationTest.java
@@ -4,11 +4,8 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 public class ApplicationTest extends TestCase {
-//    @Test
-//    public void testRun() {
-//        String expectedValue = "Hello world!";
-//        Application app = new Application();
-//        String actualValue = app.run();
-//        assertEquals(expectedValue, actualValue);
-//    }
+    @Test
+    public void testRun() {
+        assertTrue(true);
+    }
 }
